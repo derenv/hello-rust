bea28a059344064a6c084ecc1abc2e8c29312f79
Deren Vural <derenv@live.co.uk>
2021-12-17 20:11:15 +0000

    fix linting
    Signed-off-by: Deren Vural <derenv@live.co.uk>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-
-
 pub fn function_for_test2(x: i32) -> i32 {
     println!("test runs OK ya dick");
     x
--- tests/example_integration_tests.rs
+++ tests/example_integration_tests.rs
@@ -5,7 +5,7 @@ use hello_rust;
  * Integration tests
  */
 #[test]
-fn example_inegration_test(){
+fn example_inegration_test() {
     let y: i32 = 23;
-    assert_eq!(23,hello_rust::function_for_test2(y));
+    assert_eq!(23, hello_rust::function_for_test2(y));
 }
